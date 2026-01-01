ec27d0846c17fc4e8ed2704ac6b4e51df20403c5
takki-10 <jakob.takhirov@gmail.com>
2026-01-01 19:38:11 +0100

    Første utkast matteside for barn

---
+++ multiplikasjon.js
@@ -0,0 +1,60 @@
+// Globale variabler
+let tall1;
+let tall2;
+let svar ;
+const response = document.getElementById("respons") ;
+console.log("r:",response)
+
+
+// Lager en ny oppgave
+
+function nyOppgave() {
+    tall1 = Math.floor(Math.random() * 1000) + 1;
+    tall2 = Math.floor(Math.random() * 1000) + 1;
+
+    document.getElementById("tall1").textContent = tall1;
+    document.getElementById("tall2").textContent = tall2;
+
+    svar = tall1 * tall2;
+}
+
+ // console.log("x:", fasit)
+
+
+// Sjekker svaret
+
+function sjekkSvar() {
+    let input = document.getElementById("gjetttall");
+    let brukerSvar = Number(input.value);
+
+    input.value = ""; // tøm input-feltet
+
+    if (brukerSvar === svar) {
+        // alert("Du svarte riktig!");
+        console.log("Bruker skrev:", brukerSvar);
+        console.log("Riktig svar er:", svar) ;
+        nyOppgave(); // ny oppgave når svaret er riktig
+        response.textContent = "Du svarte riktig, prøv neste oppgave"
+        
+
+            
+    } else {
+        // alert("Feil, prøv igjen!");
+        console.log("Bruker skrev:", brukerSvar) ;
+        console.log("Bruker skrev", svar) ;
+        response.textContent = "Du svarte feil, prøv igjen"
+
+    }
+}
+
+
+// Enter-tast sender inn svaret
+
+document.getElementById("gjetttall").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        sjekkSvar();
+    }
+});
+
+// Start første oppgave
+nyOppgave();
